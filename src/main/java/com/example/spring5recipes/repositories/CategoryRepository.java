b1bf6bc23d8fb905eb0499423f7442e40e25f610
hackerrank <me@hackerrank.com>
2020-07-28 23:23:31 +0530

    Added JPA repository query methods for Categor and UOM tables.

--- src/main/java/com/example/spring5recipes/repositories/CategoryRepository.java
+++ src/main/java/com/example/spring5recipes/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.example.spring5recipes.repositories;
 import com.example.spring5recipes.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/example/spring5recipes/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/example/spring5recipes/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.example.spring5recipes.repositories;
 import com.example.spring5recipes.domain.UnitOfMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends JpaRepository<UnitOfMeasure,Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
