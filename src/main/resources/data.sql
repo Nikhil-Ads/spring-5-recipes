94499273602c1ce697b840fa8a4d9cc83d8c3661
hackerrank <me@hackerrank.com>
2020-07-28 22:57:30 +0530

    Added DML Data file for initialization of values in DB.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into category (description) VALUES ('American');
+insert into category (description) VALUES ('Italian');
+insert into category (description) VALUES ('Mexican');
+insert into category (description) VALUES ('Fast Food');
+
+insert into unit_of_measure (description) VALUES ('Teaspoon');
+insert into unit_of_measure (description) VALUES ('Tablespoon');
+insert into unit_of_measure (description) VALUES ('Cup');
+insert into unit_of_measure (description) VALUES ('Pinch');
+insert into unit_of_measure (description) VALUES ('Ounce');
