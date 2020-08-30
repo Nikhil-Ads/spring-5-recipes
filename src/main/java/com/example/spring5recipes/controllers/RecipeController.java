6d1f19fb5a173013b98c12f33fd1be05964b7f70
hackerrank <me@hackerrank.com>
2020-08-30 21:33:14 +0530

    Added Mapping for Creating a Recipe and a View for the same.

--- src/main/java/com/example/spring5recipes/controllers/RecipeController.java
+++ src/main/java/com/example/spring5recipes/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.example.spring5recipes.controllers;
 
+import com.example.spring5recipes.commands.RecipeCommand;
 import com.example.spring5recipes.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -24,6 +27,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 
         return "recipe/show";
+    }
 
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
     }
 }
